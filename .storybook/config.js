bded633cdcb73396d75538e9a571ed45fa9dc2cc
sygnalcode <hello@sygnal.de>
2019-09-30 14:21:25 +0200

    Installing Storybook addon info

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,7 @@
 import { configure } from '@storybook/react'
+import { addDecorator } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
+
+addDecorator(withInfo)
 
 configure(require.context('../src', true, /\.stories\.js$/), module)
