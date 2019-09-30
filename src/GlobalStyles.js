2bc8aa22949a4232e5cc3a4798cfa62da7b3af56
sygnalcode <hello@sygnal.de>
2019-09-30 13:32:54 +0200

    Adding component GlobalStyles

---
+++ src/GlobalStyles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background: #eee;
+}
+`
+export default GlobalStyle
--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import GlobalStyle from './GlobalStyles'
 
-ReactDOM.render(<App />, window.root)
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>,
+  window.root
+)
 
 serviceWorker.unregister()
