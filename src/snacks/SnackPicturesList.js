80c96648073b61e551f14a3590db5e8570ea65cd
sygnalcode <hello@sygnal.de>
2019-10-08 17:00:08 +0200

    Grid layout and eventlistener scroll

--- src/snacks/SnackPicturesList.js
+++ src/snacks/SnackPicturesList.js
@@ -1,24 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components/macro'
 import SnackPicture from './SnackPicture'
 
 export default function SnackPicturesList({ snacksData }) {
+  function GetScrollYOffset() {
+    const [scrollYPosition, setScrollYPosition] = useState(0)
+
+    useEffect(() => {
+      window.addEventListener('scroll', () =>
+        setScrollYPosition(window.pageYOffset)
+      )
+      return () =>
+        window.removeEventListener('scroll', () =>
+          setScrollYPosition(window.pageYOffset)
+        )
+    })
+    return <div>Position: {scrollYPosition}</div>
+  }
+
   return (
-    <PictureListStyled>
-      {snacksData.map((singleSnackData, index) => {
-        return <SnackPicture data={singleSnackData} key={index} />
-      })}
-    </PictureListStyled>
+    <ScrollGridStyled>
+      <div></div>
+      <PictureListStyled>
+        <GetScrollYOffset />
+        {snacksData.map((singleSnackData, index) => {
+          return <SnackPicture data={singleSnackData} key={index} />
+        })}
+      </PictureListStyled>
+      <div></div>
+    </ScrollGridStyled>
   )
 }
+
+const ScrollGridStyled = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 454px auto 1150px;
+  /* todo: Höhen der rows finetunen */
+`
 
 const PictureListStyled = styled.section`
   display: grid;
   justify-items: center;
   gap: 80px;
   overflow: scroll;
-  height: 100vh;
-  padding-top: 600px;
-  /* todo: Padding bottom o.ä. */
-  /* padding-bottom: 1150px; */
 `
