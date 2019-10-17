70f5e0143b52f7dedf36af155a846122c366f665
sygnalcode <hello@sygnal.de>
2019-10-17 12:59:10 +0200

    Setting GlobalStyles in storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,17 @@
-import { configure } from '@storybook/react'
-import { addDecorator } from '@storybook/react'
+import { addDecorator, configure } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
 
+import React from 'react'
+import GlobalStyles from '../src/common/GlobalStyles'
+
+const withGlobal = story => (
+  <React.Fragment>
+    <GlobalStyles />
+    {story()}
+  </React.Fragment>
+)
+
+addDecorator(withGlobal)
 addDecorator(withInfo)
 
 configure(require.context('../src', true, /\.stories\.js$/), module)
--- src/snacks/SnackInfos.stories.js
+++ src/snacks/SnackInfos.stories.js
@@ -1,26 +1,22 @@
 import React from 'react'
 import SnackInfos from './SnackInfos'
-import GlobalStyles from '../common/GlobalStyles'
 
 export default {
   title: 'SnackInfos'
 }
 
 export const small = () => (
-  <>
-    <GlobalStyles />
-    <SnackInfos
-      snacksData={[
-        {
-          brand: 'eat performance',
-          flavor: 'Cashew & Kirsche',
-          tags: ['raw', 'lowsugar', 'soyfree', 'bio'],
-          description:
-            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt et dolore.',
-          price: 2.0
-        }
-      ]}
-      scrollYPosition="0"
-    />
-  </>
+  <SnackInfos
+    snacksData={[
+      {
+        brand: 'eat performance',
+        flavor: 'Cashew & Kirsche',
+        tags: ['raw', 'lowsugar', 'soyfree', 'bio'],
+        description:
+          'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt et dolore.',
+        price: 2.0
+      }
+    ]}
+    scrollYPosition="0"
+  />
 )
