bbc90388ecbfced6e9e854d313dfac4dcc826fde
sygnalcode <hello@sygnal.de>
2019-10-22 17:17:29 +0200

    React router with navigation logic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React, { useState } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import styled from 'styled-components/macro'
 import Navigation from './common/Navigation'
-import SnackPicturesList from './snacks/SnackPicturesList'
-import SnackInfos from './snacks/SnackInfos'
+import SnackPage from './snacks/SnackPage'
+import UserPage from './user/UserPage'
 import originalSnacksData from './snacks/snacks.json'
+import originalUserData from './user/user.json'
 
 export default function App() {
   const [scrollYPosition, setScrollYPosition] = useState(0)
   const [snacksData, setSnacksData] = useState(originalSnacksData)
+  const [userData, setUserData] = useState(originalUserData)
 
   // prevent console warning
   snacksData === null && setSnacksData(originalSnacksData)
+  userData === null && setUserData(originalSnacksData)
 
   return (
-    <PageStyled>
-      <Navigation />
-      <SnackPicturesList
-        snacksData={snacksData}
-        setScrollYPosition={setScrollYPosition}
-      />
-      <SnackInfos snacksData={snacksData} scrollYPosition={scrollYPosition} />
-    </PageStyled>
+    <Router>
+      <PageStyled>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => <UserPage userData={userData} />}
+          />
+          <Route
+            path="/snacks"
+            render={() => (
+              <SnackPage
+                snacksData={snacksData}
+                setScrollYPosition={setScrollYPosition}
+                scrollYPosition={scrollYPosition}
+              />
+            )}
+          />
+        </Switch>
+        <Navigation />
+      </PageStyled>
+    </Router>
   )
 }
 
--- src/common/Navigation.js
+++ src/common/Navigation.js
@@ -1,16 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components/macro'
+import { NavLink } from 'react-router-dom'
 import { ChevronLeft } from 'styled-icons/boxicons-regular/ChevronLeft'
+import { ChevronRight } from 'styled-icons/boxicons-regular/ChevronRight'
 
 export default function Navigation() {
+  const [currentPage, setCurrentPage] = useState(0)
+
   return (
-    <DotStyled>
-      <ChevronLeft size="40" title="Unlock account" />
-    </DotStyled>
+    <>
+      {currentPage === 0 ? (
+        <NavLink to="/snacks" onClick={() => setCurrentPage(1)}>
+          <DotStyledRight>
+            Snacks
+            <ChevronRight size="40" title="Snacks page" />
+          </DotStyledRight>
+        </NavLink>
+      ) : (
+        <NavLink to="/" onClick={() => setCurrentPage(0)}>
+          <DotStyledLeft>
+            <ChevronLeft size="40" title="User page" />
+          </DotStyledLeft>
+        </NavLink>
+      )}
+    </>
   )
 }
 
-const DotStyled = styled.div`
+const DotStyledRight = styled.div`
+  height: 60px;
+  width: 160px;
+  background-color: #fff379;
+  box-shadow: 2px 0 15px rgba(0, 0, 0, 0.3);
+  border-radius: 30px;
+  display: inline-block;
+  position: fixed;
+  text-align: center;
+  padding-top: 11px;
+  font-size: 1.5rem;
+  z-index: 20;
+  top: 2rem;
+  right: 2rem;
+`
+
+const DotStyledLeft = styled.div`
   height: 60px;
   width: 60px;
   background-color: #fff379;
@@ -18,10 +51,10 @@ const DotStyled = styled.div`
   border-radius: 50%;
   display: inline-block;
   position: fixed;
-  top: 2rem;
-  left: 2rem;
   text-align: center;
   padding-top: 11px;
   font-size: 1.5rem;
   z-index: 20;
+  top: 2rem;
+  left: 2rem;
 `
